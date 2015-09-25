87d87330433ab9e4eb8786fb17ce4661464668ba
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2015-09-24 21:50:36 -0300

    Updates, Better concurrency and clean

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-type RequestResult struct {
+type requestResult struct {
 	Query struct {
 		Count   int    `json:"count"`
 		Created string `json:"created"`
@@ -20,26 +20,36 @@ type RequestResult struct {
 	} `json:"query"`
 }
 
-const URL = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%27www.google.com%2Ffinance%2Fconverter%3Fa%3D1%26from%3DUSD%26to%3DBRL%27%20and%20xpath%3D%27%2F%2F*%5B%40id%3D\"currency_converter_result\"%5D%2Fspan%2Ftext()%27&format=json&callback="
+const url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%27www.google.com%2Ffinance%2Fconverter%3Fa%3D1%26from%3DUSD%26to%3DBRL%27%20and%20xpath%3D%27%2F%2F*%5B%40id%3D\"currency_converter_result\"%5D%2Fspan%2Ftext()%27&format=json&callback="
 
 // Make the request and return the content of body
-func Checker() (string, error) {
-	if resp, err := http.Get(URL); err != nil {
+func checker() (string, error) {
+	resp, err := http.Get(url)
+
+	if err != nil {
 		return "", err
-	} else {
-		defer resp.Body.Close()
-		if contents, err := ioutil.ReadAll(resp.Body); err != nil {
-			return "", err
-		} else {
-			return string(contents), nil
-		}
 	}
+
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			log.Println("Error to close get request")
+		}
+	}()
+
+	contents, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(contents), nil
 }
 
 // Parses the json and get only the value Results
-func ParseJSON(jsonResult string) string {
+func parseJSON(jsonResult string) string {
 	log.Println("JSON REQUEST", jsonResult)
-	result := new(RequestResult)
+	result := new(requestResult)
 
 	err := json.Unmarshal([]byte(jsonResult), result)
 
@@ -53,15 +63,33 @@ func ParseJSON(jsonResult string) string {
 }
 
 // Checking the webservice each 30 minutes
-func Pool() chan string {
+func pool() chan string {
 	ch := make(chan string)
 	go func() {
-		for {
-			if res, err := Checker(); err == nil {
-				if jsonRes := ParseJSON(res); jsonRes != "" {
+		haveResponse := false
+		requestJSON := func() {
+			if res, err := checker(); err == nil {
+				if jsonRes := parseJSON(res); jsonRes != "" {
+					haveResponse = true
 					ch <- jsonRes
-					time.Sleep(time.Minute * 30)
 				}
+			}
+		}
+
+		for {
+			for {
+				requestJSON()
+				if haveResponse {
+					break
+				} else {
+					log.Println("No result try again ...")
+				}
+			}
+
+			c := time.Tick(30 * time.Minute)
+			for now := range c {
+				log.Println("Updated at %v", now)
+				requestJSON()
 			}
 		}
 	}()
@@ -70,31 +98,37 @@ func Pool() chan string {
 
 func main() {
 	// Get the first value
-	checkerPool := Pool()
-	latestResult := <-checkerPool
+	checkerpool := pool()
+	latestResult := <-checkerpool
 
 	// To deploy at heroku :D
 	env := os.Getenv("GO_ENV")
 	host := "127.0.0.1"
 	port := 9000
+	//token := ""
 
 	if env == "PRODUCTION" {
 		host = ""
 		port, _ = strconv.Atoi(os.Getenv("PORT"))
+		//token = os.Getenv("TOKEN")
 	}
 
 	address := fmt.Sprintf("%s:%d", host, port)
 	log.Println("Ready to serve at", address)
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
-		log.Println(len(checkerPool))
-
-		// Check if chan res a value waiting
-		if len(checkerPool) >= 1 {
-			latestResult = <-checkerPool
+		select {
+		case latestResult := <-checkerpool:
+			log.Println("Get result", latestResult)
+		default:
+			log.Println("Get cache value")
 		}
 
-		rw.Write([]byte(latestResult))
+		_, err := rw.Write([]byte(latestResult))
+
+		if err != nil {
+			log.Println(err)
+		}
 	})
 
 	if err := http.ListenAndServe(address, nil); err != nil {
